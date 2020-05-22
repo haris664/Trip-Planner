f163f5da827e590eb1c1b8c341d75187cd688da3
Haris Munir <haris4000@yahoo.com>
2020-05-21 23:24:26 -0500

    Retrieving a entered input from a mapbox api and set the bboxaccording to the provided longitude and latitude.

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,23 @@ const firstInputEle = document.querySelector('.origin-container');
 
 firstInputEle.onsubmit = event => {
   const input = event.target.querySelector('input');
-  console.log(input.value);
+  if (input.value.length > 0) {
+    startingLocation(input.value);
+  }
   event.preventDefault();
+  input.value = '';
+}
+
+function startingLocation(query) {
+  fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?limit=10&bbox=-97.325875,49.766204,-96.953987,49.99275&access_token=${key}`)
+  .then(resp => {
+    if (resp.ok) {
+      return resp.json();
+    } else {
+      throw new Error ('something not working');
+    }
+  })
+  .then(data => {
+    console.log(data);
+  })
 }
