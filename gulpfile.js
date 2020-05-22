c9c33c0c8dae01d57609b167cd68c4f6b71a3602
Haris Munir <haris4000@yahoo.com>
2020-05-21 22:45:38 -0500

    completed the gulpfile and set the watch function inorder to minify the css and js file

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,31 @@
 const gulp = require('gulp');
 const cleanCss = require('gulp-clean-css');
 const uglify = require('gulp-uglify');
+const babel1 = require('gulp-babel');
 
 function minifyCss() {
-  gulp.src('src/css/*.css')
+  return gulp.src('src/css/*.css')
   .pipe(cleanCss())
   .pipe(gulp.dest('dist/css'));
 }
 
-exports.default = minifyCss;
+function html() {
+  return gulp.src('src/index.html')
+  .pipe(gulp.dest('dist'))
+}
+
+function transpileJs() {
+ return gulp.src('src/js/app.js')
+  .pipe(babel1({
+    presets: ['@babel/env']
+  }))
+  .pipe(uglify())
+  .pipe(gulp.dest('dist/js'))
+} 
+
+function watch() {
+  gulp.watch('src/css/*.css',minifyCss);
+  gulp.watch('src/js/app.js',transpileJs);
+}
+
+exports.default = gulp.series(html,minifyCss,transpileJs,watch);
